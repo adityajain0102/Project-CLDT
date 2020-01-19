92a711c22f60b52ca945615ac0524fa2f901548d
N.Adityavardhan <nagamalliaditya3@gmail.com>
2020-01-19 13:35:27 +0530

    Initial project setup , userinput via cmd

---
+++ server/utils/message.js
@@ -0,0 +1,12 @@
+/*
+    created_by: Adityavardhan.N
+    created_at: 19-01-2020: 12:25 AM
+    Desc: Shows the Init message , and deals with all messages based on user choice 
+*/
+
+var Message = {
+    INIT_MESSAGE: 'Welcome to Command Line Dictionary tool \n for help :  ./dict --help '
+};
+
+//export the Message
+module.exports = Message;
